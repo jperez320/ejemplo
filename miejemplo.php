0cc6c21115f64b5b403f04f38305cc02e70fe67d
Jose Perez <jperez820@hotmail.com>
2018-01-28 21:27:50 -0400

    Inicio del proyecto

---
+++ miejemplo.php
@@ -0,0 +1,13 @@
+<?php  ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Emeplo de un repositorio con github</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	EJEMPLO DE MI REPOSITORIO
+</body>
+</html>
